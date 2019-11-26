eb825d20c2111a31995293b0daa29791f8d26217
peebbv6364 <peebbv6364@gmail.com>
2019-11-26 22:12:22 +0900

    4.15~4.18

--- fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise15/Fibonacci.kt
+++ fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise15/Fibonacci.kt
@@ -2,26 +2,37 @@ package com.fpinkotlin.recursion.exercise15
 
 
 fun <T> List<T>.head(): T =
-    if (this.isEmpty())
-        throw IllegalArgumentException("head called on empty list")
-    else
-        this[0]
+        if (this.isEmpty())
+            throw IllegalArgumentException("head called on empty list")
+        else
+            this[0]
 
 fun <T> List<T>.tail(): List<T> =
-    if (this.isEmpty())
-        throw IllegalArgumentException("tail called on empty list")
-    else
-        this.subList(1, this.size)
+        if (this.isEmpty())
+            throw IllegalArgumentException("tail called on empty list")
+        else
+            this.subList(1, this.size)
 
 fun <T, U> foldLeft(list: List<T>, z: U, f: (U, T) -> U): U {
     tailrec fun foldLeft_(list: List<T>, acc: U, f: (U, T) -> U): U =
-        if (list.isEmpty())
-            acc
-        else
-            foldLeft_(list.tail(), f(acc, list.head()), f)
+            if (list.isEmpty())
+                acc
+            else
+                foldLeft_(list.tail(), f(acc, list.head()), f)
     return foldLeft_(list, z, f)
 }
 
-fun fibo(number: Int): String = TODO("fibo")
+fun fibo(number: Int): String {
+    tailrec fun fibo(index: Int, acc: String, previousValues: Pair<Int, Int>): String =
+            when {
+                number == index -> acc
+                index == 0 -> fibo(index + 1, "0", Pair(1, 0))
+                else -> {
+                    val currentValue = previousValues.first + previousValues.second
+                    fibo(index + 1, makeString(listOf(acc, currentValue), ","), Pair(previousValues.second, currentValue))
+                }
+            }
+    return fibo(0, "", Pair(0, 0))
+}
 
-fun <T> makeString(list: List<T>, separator: String): String = TODO("makeString")
+fun <T> makeString(list: List<T>, separator: String): String = foldLeft(list.tail(), list.head().toString()) { acc, item -> acc + separator + item.toString() }
--- fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise16/Iterate.kt
+++ fpinkotlin-parent/fpinkotlin-recursion-exercises/src/main/kotlin/com/fpinkotlin/recursion/exercise16/Iterate.kt
@@ -1,4 +1,13 @@
 package com.fpinkotlin.recursion.exercise16
 
 
-fun <T> iterate(seed: T, f: (T) -> T, n: Int): List<T> = TODO("iterate")
+fun <T> iterate(seed: T, f: (T) -> T, n: Int): List<T> {
+    tailrec fun iterate(currentSeed: T, currentIndex: Int, acc: MutableList<T>): List<T> =
+            if (currentIndex == 0) {
+                acc
+            } else {
+                iterate(f(currentSeed), currentIndex - 1, acc.apply { add(currentSeed) })
+            }
+
+    return iterate(seed, n, mutableListOf())
+}
